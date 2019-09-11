219a1ea2099d279670899396c8d9b24da205134c
FSimas92 <fs6806a@student.american.edu>
2019-09-11 17:22:08 -0400

    Week 3 additions

---
+++ Week 1 Assignment/Assets/_Scripts/CannonControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonControl : MonoBehaviour
+{
+    public GameObject piggyPlayer;
+    public float strength = 500; //the scalar that defines the strength of the cannon launch
+    Vector3 direction;
+    const int MAX_ANGLE = 80;
+    const int MIN_ANGLE = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        direction = mousePositionInWorld - transform.position;
+
+        float alpha = Mathf.Acos(Vector3.Dot(Vector3.right, direction.normalized)) * Mathf.Rad2Deg;
+
+        if (alpha < MAX_ANGLE && alpha > MIN_ANGLE && direction.y > 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, alpha));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            piggyPlayer.transform.parent = null;
+            piggyPlayer.GetComponent<Rigidbody2D>().gravityScale = 1;
+            piggyPlayer.GetComponent<Rigidbody2D>().AddForce(direction * strength);
+        }
+    }
+}
---
+++ Week 1 Assignment/Assets/_Scripts/PiggyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiggyController : MonoBehaviour
+{
+    Vector3 startPosition;
+    Quaternion startRotation;
+    Transform cannon;
+    const int waitTime = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+        cannon = transform.parent;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ScoreManager.instance.score++;
+
+        StartCoroutine("ResetPiggy");
+    }
+
+    IEnumerator ResetPiggy()
+    {
+        yield return new WaitForSeconds(waitTime);
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        transform.parent = cannon.transform;
+    }
+}
