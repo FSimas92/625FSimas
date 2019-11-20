ea3accff66230ebe2777e499acd960b70402c706
FSimas92 <fs6806a@student.american.edu>
2019-11-20 14:51:11 -0500

    NPC and VR Assignments

---
+++ NPC AI Assignment/Assets/EnemyScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject moveSpot;
+    public GameObject moveSpot2;
+    Transform player;
+
+    enum NPC_STATES { Patrol, Chase };
+    NPC_STATES state;
+
+    Vector3 destination;
+
+    RaycastHit hit;
+    Vector3 rayDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = NPC_STATES.Patrol;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        destination = moveSpot.transform.position;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rayDirection = (player.transform.position - transform.position);
+        bool raycastdown = Physics.Raycast(transform.position, rayDirection, out hit);
+
+        if (raycastdown && hit.transform.name.Equals("Player"))
+        {
+            state = NPC_STATES.Chase;
+        }
+        else
+        {
+            state = NPC_STATES.Patrol;
+        }
+
+        switch (state)
+        {
+            case NPC_STATES.Chase:
+
+                agent.SetDestination(player.position);
+
+                break;
+
+            case NPC_STATES.Patrol:
+
+                agent.SetDestination(destination);
+                if (Vector3.Distance(transform.position, destination) < 0.4f)
+                {
+                    Patrolling();
+                }
+
+                break;
+        }
+        Debug.Log(state);
+    }
+
+    void Patrolling()
+    {
+        if (Vector3.Distance(transform.position, moveSpot.transform.position) < 0.4f)
+        {
+            destination = moveSpot2.transform.position;
+            Debug.Log("moving to 2");
+        }
+        else
+        {
+            destination = moveSpot.transform.position;
+            Debug.Log("moving to 1");
+        }
+    }
+}
---
+++ NPC AI Assignment/Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    float camLookAhead = 6f;
+    float camFollowBehind = 12f;
+    float camFollowAbove = 8f;
+    float rotateSpeed = 60f;
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 cameraPosition = transform.position + (Vector3.up * camFollowAbove) + (-transform.forward * camFollowBehind);
+        Camera.main.transform.position = cameraPosition;
+        Camera.main.transform.LookAt(transform.position + transform.forward * camLookAhead);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+             cc.Move(transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            cc.Move(-transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            cc.Move(-transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            cc.Move(transform.right * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            Debug.Log("Player detected");
+        }
+    }
+}
